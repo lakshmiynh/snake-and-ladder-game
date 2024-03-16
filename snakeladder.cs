2e41b2ce8032654d2396b7b9d7270527ab28b46a
Lakshmi <lakshmiynh8@gmail.com>
2024-03-16 10:16:56 +0530

    adedd:single player

---
+++ snakeladder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeandladdergame
+{
+     class snakeladder
+    {
+        public void Startgame()
+        {
+            int[] snake = { 16, 47, 49, 56, 62, 64, 87, 93, 95, 98 };
+            
+            int[] ladder = { 1, 4, 9, 21, 28, 36, 51, 71, 80 };
+            
+
+            Console.WriteLine("Start the snake and ladder game");
+
+            int user1dicevalue = 0; // user1 position 
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            snakeladder game=new snakeladder();
+            game.Startgame();
+
+        }
+
+    }
+}
