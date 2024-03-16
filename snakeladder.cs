e5918ec3016243d8ed58cb10499174fa6b0ca3d2
Lakshmi <lakshmiynh8@gmail.com>
2024-03-16 10:31:28 +0530

    added:function checks for optin they are no play,ladder or snake

--- snakeladder.cs
+++ snakeladder.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace snakeandladdergame
 {
-     class snakeladder
+    class snakeladder
     {
         public void Startgame()
         {
             int[] snake = { 16, 47, 49, 56, 62, 64, 87, 93, 95, 98 };
-            
+
             int[] ladder = { 1, 4, 9, 21, 28, 36, 51, 71, 80 };
-            
+
 
             Console.WriteLine("Start the snake and ladder game");
 
@@ -27,16 +27,84 @@ namespace snakeandladdergame
 
             }
 
+            int user2dicevalue = 0;
 
 
+            while (true)
+            {
+                //Console.WriteLine("who is playing press 1 for user1 and 2 user2");
+                int user = 0;
+                // int user=Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+                if (user == 0)
+                {
+                    Console.WriteLine("press any key to roll the dice ");
+
+                    user1dicevalue = Rolldice();
+
+
+                    for (int i = 0; i <= snake.Length - 1; i++)
+                    {
+                        if (user1dicevalue == snake[i])
+                        {
+                            user1dicevalue -= 10;
+                        }
+                    }
+
+                    for (int j = 0; j <= ladder.Length - 1; j++)
+                    {
+                        if (user1dicevalue == ladder[j])
+                        {
+                            user1dicevalue += 10;
+                        }
+                    }
+
+
+                    user++;
+                }
+
+
+
+                else
+                {
+                    Console.WriteLine("press any key to roll the dice ");
+
+                    user2dicevalue = Rolldice();
+
+
+                    for (int i = 0; i <= snake.Length - 1; i++)
+                    {
+                        if (user2dicevalue == snake[i])
+                        {
+                            user1dicevalue -= 10;
+                        }
+                    }
+
+                    for (int j = 0; j <= ladder.Length - 1; j++)
+                    {
+                        if (user2dicevalue == ladder[j])
+                        {
+                            user2dicevalue += 10;
+                        }
+                    }
+
+
+                    user--;
+                }
+
+
+            }
+            static void Main(string[] args)
+            {
+                snakeladder game = new snakeladder();
+                game.Startgame();
+
+            }
 
         }
-        static void Main(string[] args)
-        {
-            snakeladder game=new snakeladder();
-            game.Startgame();
-
-        }
-
     }
 }
