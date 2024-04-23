d526d6f9701769c660433a6349ec01e04652d1d8
lakshmiynh <119153376+lakshmiynh@users.noreply.github.com>
2024-04-23 09:31:22 +0530

    Update snakeladder.cs
    Added:Functionality to get random dice number

--- snakeladder.cs
+++ snakeladder.cs
@@ -11,12 +11,8 @@ namespace snakeandladdergame
         public void Startgame()
         {
             int[] snake = { 16, 47, 49, 56, 62, 64, 87, 93, 95, 98 };
-            
             int[] ladder = { 1, 4, 9, 21, 28, 36, 51, 71, 80 };
-            
-
             Console.WriteLine("Start the snake and ladder game");
-
             int user1dicevalue = 0; // user1 position 
 
             int Rolldice()
@@ -24,19 +20,12 @@ namespace snakeandladdergame
                 Random random = new Random();
                 int rolledvalues = random.Next(1, 7);
                 return rolledvalues;
-
             }
-
-
-
-
         }
         static void Main(string[] args)
         {
             snakeladder game=new snakeladder();
             game.Startgame();
-
         }
-
     }
 }
