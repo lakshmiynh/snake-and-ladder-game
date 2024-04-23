4681abe8f8bb119c9660165f2b73b0b766e15090
lakshmiynh <119153376+lakshmiynh@users.noreply.github.com>
2024-04-23 09:43:58 +0530

    Update snakeladder.cs
    Added:functionality to Repeat till the player reaches the winning position 100.

--- snakeladder.cs
+++ snakeladder.cs
@@ -11,42 +11,25 @@ namespace snakeandladdergame
         public void Startgame()
         {
             int[] snake = { 16, 47, 49, 56, 62, 64, 87, 93, 95, 98 };
-
             int[] ladder = { 1, 4, 9, 21, 28, 36, 51, 71, 80 };
-
-
             Console.WriteLine("Start the snake and ladder game");
-
             int user1dicevalue = 0; // user1 position 
-
             int Rolldice()
             {
                 Random random = new Random();
                 int rolledvalues = random.Next(1, 7);
                 return rolledvalues;
-
             }
-
             int user2dicevalue = 0;
-
-
             while (true)
             {
                 //Console.WriteLine("who is playing press 1 for user1 and 2 user2");
                 int user = 0;
                 // int user=Convert.ToInt32(Console.ReadLine());
-
-
-
-
-
                 if (user == 0)
                 {
                     Console.WriteLine("press any key to roll the dice ");
-
                     user1dicevalue = Rolldice();
-
-
                     for (int i = 0; i <= snake.Length - 1; i++)
                     {
                         if (user1dicevalue == snake[i])
@@ -66,20 +49,12 @@ namespace snakeandladdergame
                     {
                         Console.WriteLine("won the game user1 ");
                     }
-
-
                     user++;
                 }
-
-
-
                 else
                 {
                     Console.WriteLine("press any key to roll the dice ");
-
                     user2dicevalue = Rolldice();
-
-
                     for (int i = 0; i <= snake.Length - 1; i++)
                     {
                         if (user2dicevalue == snake[i])
@@ -99,12 +74,8 @@ namespace snakeandladdergame
                     {
                         Console.WriteLine("won the game user2 ");
                     }
-
-
                     user--;
                 }
-
-
             }
             static void Main(string[] args)
             {
@@ -112,7 +83,6 @@ namespace snakeandladdergame
                 game.Startgame();
 
             }
-
         }
     }
 }
