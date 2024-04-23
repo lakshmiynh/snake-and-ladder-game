4a2487d60bdbeee8e5e5bb3b2a53a911238b3dd7
lakshmiynh <119153376+lakshmiynh@users.noreply.github.com>
2024-04-23 09:21:09 +0530

    Update snakeladder.cs
    Added:single player

--- snakeladder.cs
+++ snakeladder.cs
@@ -11,23 +11,16 @@ namespace snakeandladdergame
         public void Startgame()
         {
             int[] snake = { 16, 47, 49, 56, 62, 64, 87, 93, 95, 98 };
-            
             int[] ladder = { 1, 4, 9, 21, 28, 36, 51, 71, 80 };
             
-
             Console.WriteLine("Start the snake and ladder game");
-
             int user1dicevalue = 0; // user1 position 
-
-
-
         }
+          
         static void Main(string[] args)
         {
             snakeladder game=new snakeladder();
             game.Startgame();
-
         }
-
     }
 }
