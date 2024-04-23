456e6c7f2ad300f64ef84e184e902f801ed4e9b1
lakshmiynh <119153376+lakshmiynh@users.noreply.github.com>
2024-04-23 09:39:01 +0530

    Update snakeladder.cs
    Added:functionality to check for a option they are no play,ladder or snake.

--- snakeladder.cs
+++ snakeladder.cs
@@ -11,12 +11,8 @@ namespace snakeandladdergame
         public void Startgame()
         {
             int[] snake = { 16, 47, 49, 56, 62, 64, 87, 93, 95, 98 };
-
             int[] ladder = { 1, 4, 9, 21, 28, 36, 51, 71, 80 };
-
-
             Console.WriteLine("Start the snake and ladder game");
-
             int user1dicevalue = 0; // user1 position 
 
             int Rolldice()
@@ -24,29 +20,18 @@ namespace snakeandladdergame
                 Random random = new Random();
                 int rolledvalues = random.Next(1, 7);
                 return rolledvalues;
-
             }
 
             int user2dicevalue = 0;
-
-
             while (true)
             {
                 //Console.WriteLine("who is playing press 1 for user1 and 2 user2");
                 int user = 0;
                 // int user=Convert.ToInt32(Console.ReadLine());
-
-
-
-
-
                 if (user == 0)
                 {
                     Console.WriteLine("press any key to roll the dice ");
-
                     user1dicevalue = Rolldice();
-
-
                     for (int i = 0; i <= snake.Length - 1; i++)
                     {
                         if (user1dicevalue == snake[i])
@@ -62,20 +47,12 @@ namespace snakeandladdergame
                             user1dicevalue += 10;
                         }
                     }
-
-
                     user++;
                 }
-
-
-
                 else
                 {
                     Console.WriteLine("press any key to roll the dice ");
-
                     user2dicevalue = Rolldice();
-
-
                     for (int i = 0; i <= snake.Length - 1; i++)
                     {
                         if (user2dicevalue == snake[i])
@@ -91,20 +68,14 @@ namespace snakeandladdergame
                             user2dicevalue += 10;
                         }
                     }
-
-
                     user--;
                 }
-
-
             }
             static void Main(string[] args)
             {
                 snakeladder game = new snakeladder();
                 game.Startgame();
-
             }
-
         }
     }
 }
